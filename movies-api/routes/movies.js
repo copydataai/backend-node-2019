560be1dc9646d3ea00ae223d275ac84a7262bd8e
copydataai <copydataai@gmail.com>
2021-09-25 11:20:58 -0400

    Methods idempotents from CRUD

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -10,8 +10,54 @@ function moviesApi(app) {
     try {
       const data = await Promise.resolve(moviesMock);
       res.status(200).json({
-        data: moviesMock,
+        data: data,
         message: 'movies Listend',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.get('/:movieId', async function (req, res, next) {
+    try {
+      const data = await Promise.resolve(moviesMock[0]);
+      res.status(200).json({
+        data: data,
+        message: 'movie retrieve',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+  router.post('/', async function (req, res, next) {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: createdMovieId,
+        message: 'movie Create',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+  router.put('/:movieId', async function (req, res, next) {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'movies updated',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:movieId', async function (req, res, next) {
+    try {
+      const deleteMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: deleteMovieId,
+        message: 'movies deleted',
       });
     } catch (error) {
       next(error);
