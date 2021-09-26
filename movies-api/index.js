6e1c72007c6ad00e3af49c5cf65c919e4b97eba8
copydataai <copydataai@gmail.com>
2021-09-26 10:20:12 -0400

    Add Middlewares

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const app = express();
-const router = require('./routes/movies');
 
 const { config } = require('./config');
 const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
 app.use(express.json());
 moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Listening in http://localhost:${config.port}`);
